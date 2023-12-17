fa4e3e3f19d1a742327b9b64d499befd142c0d18
Darren Chin <d.chin1@icloud.com>
2023-12-17 20:21:32 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
-uvicorn main:app --reload --host "::0" --port 8000 # 2>stderr.log 1>stdout.log &
+if [ -z "${DNS_API_SECRET_KEY}" ]; then
+  echo "DNS_API_SECRET_KEY needs to be set."
+  echo "Generate a key with 'openssl rand -hex 32'"
+  exit 1
+fi
+
+if [ -z "${CF_TOKEN}" ]; then
+  echo "CF_TOKEN needs to be set."
+fi
+
+if [ -z "${CF_ZONE_ID}"]; then
+  echo "CF_ZONE_ID needs to be set."
+fi
+
+uvicorn main:app --reload --host "::0" --port 8000 2>stderr.log 1>stdout.log &
